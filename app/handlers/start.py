56895fea3e8a9b948662bdcf784a28ef720d6d5e
vladelo <vlad.lakhtion@gmail.com>
2025-08-10 19:25:32 +0300

    Small fixes

--- app/handlers/start.py
+++ app/handlers/start.py
@@ -1,7 +1,10 @@
 from aiogram import Dispatcher
-from aiogram.types import Message
+from aiogram.types import Message, CallbackQuery
 from aiogram.filters import Command
 
+from app.keyboards.start_keyboard import start_keyboard
+from app.dependencies import db_session
+from app.db_utils.player import get_or_create_player
 from app.logger import setup_logger
 
 logger = setup_logger(__name__)
@@ -11,24 +14,22 @@ async def start_command(message: Message) -> None:
     """
     Обрабатывает команду /start
     - Регистрирует или обновляет информацию об игроке.
-    - Отправляет приветственное сообщение.
+    - Отправляет приветственное сообщение с inline-кнопка для дальнейшей навигации.
 
     :param message: Объект входящего сообщения от пользователя.
     """
     logger.info(f"👋 Игрок @{message.from_user.username} запустил бота!")
 
-    # with db_session() as db:
-    #     register_or_update_player(db, telegram_id=str(message.from_user.id), username=message.from_user.username)
+    with db_session() as db:
+        get_or_create_player(db, tg_id=str(message.from_user.id), tg_username=message.from_user.username)
 
-    await message.answer("Привет!")
+    await message.answer("Привет!", reply_markup=start_keyboard())
 
 
 def register_handler(dp: Dispatcher) -> None:
     """
     Регистрирует обработчики команд и кнопок:
     - /start — запуск бота
-    - '🏠 Главное меню' — возврат в главное меню
-    - '🚓 Правила игры' — показ правил игры
 
     :param dp: Объект диспетчера aiogram.
     """
