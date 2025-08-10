c16fe25eedabf415e3a139e56ec84e82cb27265d
vladelo <vlad.lakhtion@gmail.com>
2025-08-10 19:36:40 +0300

    Small fixes

--- app/database.py
+++ app/database.py
@@ -1,8 +1,10 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from app.config import DATABASE_URL
-from app.models.base import Base
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+db_path = os.path.join(BASE_DIR, "db.sqlite3")
+database_url = f"sqlite:///{db_path}"
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(database_url, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
