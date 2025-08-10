3e46c2b1523e27ea19d07a4324e4b8369eca9401
vladelo <vlad.lakhtion@gmail.com>
2025-08-10 12:56:11 +0300

    Adding inits

---
+++ app/models/__init__.py
@@ -0,0 +1,5 @@
+from app.models.base import Base
+from app.models.player import Player
+from app.models.song import Song
+from app.models.song_stat import SongStat
+from app.models.game_session import GameSession
