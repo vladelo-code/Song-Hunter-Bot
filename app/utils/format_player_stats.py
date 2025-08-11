298eda1075fd67517d4f45c1eda79b6ce01b6907
vladelo <vlad.lakhtion@gmail.com>
2025-08-11 23:03:23 +0300

    Del time in stat (not interested)

--- app/utils/format_player_stats.py
+++ app/utils/format_player_stats.py
@@ -12,6 +12,6 @@ def format_player_stats(total_games: int, total_score: int, first_seen, last_see
         f"<b>📊 Ваша статистика:</b>\n\n"
         f"• Всего игр: <b>{total_games}</b>\n"
         f"• Общий счёт: <b>{total_score}</b>\n"
-        f"• Первый запуск: <b>{first_seen.strftime('%d.%m.%Y %H:%M')}</b>\n"
-        f"• Последняя активность: <b>{last_seen.strftime('%d.%m.%Y %H:%M')}</b>"
+        f"• Первый запуск: <b>{first_seen.strftime('%d.%m.%Y')}</b>\n"
+        f"• Последняя активность: <b>{last_seen.strftime('%d.%m.%Y')}</b>"
     )
