46ba796c24284e7143e23a5229e33888f80ecf7f
vladelo <vlad.lakhtion@gmail.com>
2025-08-11 12:04:17 +0300

    New button in keyboard

--- app/keyboards/start_keyboard.py
+++ app/keyboards/start_keyboard.py
@@ -7,5 +7,6 @@ def start_keyboard() -> InlineKeyboardMarkup:
         [InlineKeyboardButton(text="📖 Правила игры", callback_data="rules")],
         [InlineKeyboardButton(text="👤 Мой профиль", callback_data="profile")],
         [InlineKeyboardButton(text="🏆 Рейтинг", callback_data="rating")],
+        [InlineKeyboardButton(text="📈 Статистика треков", callback_data="stats_songs")],
     ])
     return keyboard
