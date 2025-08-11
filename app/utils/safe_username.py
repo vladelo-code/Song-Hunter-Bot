a4f9a93cddd4a2fcc87b6c62814d8ee9ff7d54b8
vladelo <vlad.lakhtion@gmail.com>
2025-08-11 13:48:08 +0300

    Adding docstrings

--- app/utils/safe_username.py
+++ app/utils/safe_username.py
@@ -2,4 +2,11 @@ from app.messages.texts import NO_USERNAME_USER
 
 
 def get_safe_username(username: str | None) -> str:
+    """
+    Возвращает безопасное отображение имени пользователя.
+    Если username равен None или пустой строке, возвращает значение по умолчанию.
+
+    :param username: Имя пользователя или None.
+    :return: Имя пользователя или строка-заглушка, если username отсутствует.
+    """
     return username or NO_USERNAME_USER
