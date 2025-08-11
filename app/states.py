c82050455ed84af281483506c62a56d9e217dbbf
vladelo <vlad.lakhtion@gmail.com>
2025-08-11 13:48:36 +0300

    Adding docstrings

--- app/states.py
+++ app/states.py
@@ -2,4 +2,8 @@ from aiogram.fsm.state import State, StatesGroup
 
 
 class GameStates(StatesGroup):
+    """
+    Класс состояний для игры.
+    Используется для управления состояниями FSM в процессе игры.
+    """
     WAIT_ANSWER = State()
